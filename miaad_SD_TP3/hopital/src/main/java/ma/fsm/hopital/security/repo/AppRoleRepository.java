691c53bb2f5c5ad0dd6fc3d56ee431688a7c89c8
Meryemeilla <165611188+Meryemeilla@users.noreply.github.com>
2024-05-06 15:57:23 +0200

    Add files via upload

---
+++ miaad_SD_TP3/hopital/src/main/java/ma/fsm/hopital/security/repo/AppRoleRepository.java
@@ -0,0 +1,8 @@
+package ma.fsm.hopital.security.repo;
+
+import ma.fsm.hopital.security.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppRoleRepository extends JpaRepository<AppRole, String> {
+
+}
