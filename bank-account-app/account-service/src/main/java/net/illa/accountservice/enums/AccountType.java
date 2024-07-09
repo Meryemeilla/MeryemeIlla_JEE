8c26a16a1477c2303ba50c261076f0041d8019ba
Meryemeilla <165611188+Meryemeilla@users.noreply.github.com>
2024-07-09 17:03:48 +0200

    Delete bank-account-app directory

--- bank-account-app/account-service/src/main/java/net/illa/accountservice/enums/AccountType.java
+++
@@ -1,5 +0,0 @@
-package net.illa.accountservice.enums;
-
-public enum AccountType {
-    CURRENT_ACCOUNT, SAVING_ACCOUNT
-}
--- bank-account-app/account-service/src/main/java/net/illa/accountservice/repository/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package net.illa.accountservice.repository;
-
-import net.illa.accountservice.entities.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-}
